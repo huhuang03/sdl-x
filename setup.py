4314837d69a2474f0d2a61acf2d0ca3cdfe8d265
胡文凡 <wenfan.hu@infaith.com.cn>
2023-05-24 16:53:11 +0800

    up doc

--- setup.py
+++ setup.py
@@ -1,14 +1,10 @@
-from setuptools import setup, find_packages
-
-packages = find_packages()
-packages.remove('samples')
-packages.remove('test')
+from setuptools import setup
 
 setup(
     name='sdl',
-    version='1.0.0',
-    description='收集的简单deep learning方法',
-    author="th",
-    packages=packages,
+    version='0.0.1',
+    description='collected simple deep learning functions',
+    author="huhuang03",
+    packages=['sdl'],
     install_requires=['numpy']
 )
