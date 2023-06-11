ece914d47efdb5b5cb34a9d734368d9557a510dc
huhuang03 <huhuang03@gmail.com>
2023-06-11 22:39:39 +0800

    add version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sdl-x',
-    version='0.0.1',
+    version='0.0.2',
     description='collected simple deep learning functions',
     author="huhuang03",
     packages=['sdl_x'],
