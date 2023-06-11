fix: fix numerical_gradient

test/gradient_test.py
@@ -13,7 +13,8 @@ def x2_plus_x2(x):
 
 class GradientTest(unittest.TestCase):
 
-    def test_numerical_gradient(self):
+    @staticmethod
+    def test_numerical_gradient():
         w = np.random.randn(28 * 28, 10)
         b = np.random.randn(1, 10)
         x = np.random.randn(1, 28 * 28) * 255
@@ -22,24 +23,7 @@ class GradientTest(unittest.TestCase):
         manually_dw = numerical_gradient(loss, w)
         math_dw = x.T @ numpy.ones((1, 10))
         assert_same(manually_dw, math_dw)
-        # calc by manually
 
-        # gradient = numerical_gradient(loss, w)
-        # print(gradient)
-    # def test_numerical_gradient_dim_2(self):
-    #     f = lambda x: 2 * math.pow(x[0], 2) + 3 * math.pow(x[1], 2)
-    #     gradient = numerical_gradient(f, np.array([3.0, 4.0]))
-    #     assert self._check_equal_allow_small_diff(gradient, np.array([12., 24.]))
-    #
-    # def _check_equal_allow_small_diff(self, arr1, arr2):
-    #     allow_diff = 1e-5
-    #     return np.all((arr1 - arr2) < allow_diff)
-    #
-    # def test_numerical_gradient(self):
-    #     # should not be like this.
-    #     gradient = numerical_gradient(x2_plus_x2, np.array([[3.0], [4.0]]))
-    #     self._check_equal_allow_small_diff(gradient, np.array([[6.], [8.]]))
-    #
     # def test_gradient_decline(self):
     #     y =  gradient_decline(x2_plus_x2, np.array([5, 8]), 0.01)
     #     self.assertTrue(y < 1e-30)